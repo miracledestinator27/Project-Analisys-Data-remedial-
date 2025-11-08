c7f0b5138cb89eef1c716761215ab91d3057e022
miracledestinator27 <b25b9d037@student.devacademy.id>
2025-11-08 22:26:27 +0700

    Implement data processing and Streamlit header
    Added data processing for shipping limit dates and created a Streamlit dashboard header.

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-sns.set(style='dark')
 # Membaca data (pastikan Anda sudah memiliki file data yang sesuai)
 customers_df = pd.read_csv("E-commerce-public-dataset\E-Commerce Public Dataset\customers_dataset.csv")
 geolocation_df = pd.read_csv("E-commerce-public-dataset\E-Commerce Public Dataset\geolocation_dataset.csv")
@@ -16,6 +15,11 @@ category_df = pd.read_csv("dE-commerce-public-dataset\E-Commerce Public Dataset\
 product_df = pd.read_csv("E-commerce-public-dataset\E-Commerce Public Dataset\products_dataset.csv")
 sellers_df = pd.read_csv("E-commerce-public-dataset\E-Commerce Public Dataset\sellers_dataset.csv")
 
+# --- Data Processing ---
+order_items_df['shipping_limit_date'] = pd.to_datetime(order_items_df['shipping_limit_date'])
+order_items_df['month'] = order_items_df['shipping_limit_date'].dt.strftime('%B')
+order_items_df['year'] = order_items_df['shipping_limit_date'].dt.year
+order_items_df['month_num'] = order_items_df['shipping_limit_date'].dt.month
 
 # Membuat class DataAnalyzer untuk analisis data
 class DataAnalyzer:
@@ -67,6 +71,8 @@ end_date = pd.to_datetime(end_date)
 # Filter data berdasarkan data input
 main_df = daily_metrics_df[(daily_metrics_df['order_purchase_timestamp'] >= start_date) & 
                             (daily_metrics_df['order_purchase_timestamp'] <= end_date)]
+
+# --- Streamlit Dashboard ---
 
 # Streamlit header
 st.header('E-commerce Dashboard')
