6afc6d45bebf40a6c744cf3a6c05f232ea93db04
miracledestinator27 <b25b9d037@student.devacademy.id>
2025-11-08 22:34:17 +0700

    Fix date input sidebar formatting in Dashboard.py

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -39,13 +39,13 @@ orders_df = pd.read_csv("orders_dataset.csv")
 orders_df.head()
 daily_metrics_df = create_daily_metrics_df(orders_df)
 
-
 # sidebar untuk input tanggal
 with st.sidebar:
-    start_date, end_date = st.date_input()
-        label='date range',
+    start_date, end_date = st.date_input(
+        label='Date Range',
         value=(daily_metrics_df['order_purchase_timestamp'].min().date(), 
             daily_metrics_df['order_purchase_timestamp'].max().date())
+    )
 
 # menggonversi input tanggal ke datetime
 start_date = pd.to_datetime(start_date)
