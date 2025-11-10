bf279199365591a7b05ba419bcc339b2014630fd
miracledestinator27 <b25b9d037@student.devacademy.id>
2025-11-11 01:55:43 +0700

    Update Dashboard2.py

--- Dashboard2.py
+++ Dashboard2.py
@@ -290,7 +290,7 @@ def plot_brazil_map(data):
     ax.scatter(
         data["geolocation_lng"],
         data["geolocation_lat"],
-        s=20,
+        s=2,
         alpha=0.8,
         color='yellow',
         edgecolor='black',
@@ -321,6 +321,7 @@ st.download_button(
 )
 
 st.success("✅ Dashboard berhasil dijalankan sepenuhnya!")
+
 
 
 
