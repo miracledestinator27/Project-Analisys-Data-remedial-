73f143619999d7e431537755cfc1efe57deff0a9
miracledestinator27 <b25b9d037@student.devacademy.id>
2025-11-11 01:31:42 +0700

    Update Dashboard2.py

--- Dashboard2.py
+++ Dashboard2.py
@@ -285,7 +285,7 @@ def plot_brazil_map(data):
     with urllib.request.urlopen(url) as u:
         brazil = mpimg.imread(u, 'jpg')
 
-    fig, ax = plt.subplots(figsize=(8, 8))
+    fig, ax = plt.subplots(figsize=(2, 2))
     ax.imshow(brazil, extent=[-65, -24, -24, 5], zorder=1)
     ax.scatter(
         data["geolocation_lng"],
@@ -306,6 +306,7 @@ def plot_brazil_map(data):
 
 fig_map = plot_brazil_map(customers_silver.drop_duplicates(subset='customer_unique_id'))
 st.pyplot(fig_map)
+
 
 
 
