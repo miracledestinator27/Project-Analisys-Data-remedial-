d6e2efe9a491462dcc45eb2550fc239a7d5c0a5a
miracledestinator27 <b25b9d037@student.devacademy.id>
2025-11-11 00:40:27 +0700

    Update Dashboard2.py

--- Dashboard2.py
+++ Dashboard2.py
@@ -38,146 +38,9 @@ monthly_sales_df = order_items_df.groupby(['year', 'month_num', 'month']).agg({
 # Streamlit header
 st.header('E-commerce Dashboard')
 
-# Add text or descriptions
-st.write("**This is a dashboard for analyzing E-Commerce public data.**")
+# --- Judul Halaman ---
+st.title("Visualisasi Rata-rata Transaksi per Kota ")
 
-# Daily Orders Delivered
-st.subheader("Daily Orders Delivered")
-col1, col2 = st.columns(2)
-
-with col1:
-    total_order = orders_df["order_count"].sum()
-    st.markdown(f"Total Order: **{total_order}**")
-
-with col2:
-    total_revenue = orders_df["revenue"].sum()
-    st.markdown(f"Total Revenue: **{total_revenue}**")
-
-fig, ax = plt.subplots(figsize=(12, 6))
-sns.lineplot(
-    x=orders_df["order_approved_at"],
-    y=orders_df["order_count"],
-    marker="o",
-    linewidth=2,
-    color="#90CAF9"
-)
-ax.tick_params(axis="x", rotation=45)
-ax.tick_params(axis="y", labelsize=15)
-st.pyplot(fig)
-
-# Customer Spend Money
-st.subheader("Customer Spend Money")
-col1, col2 = st.columns(2)
-
-with col1:
-    total_spend = sum_spend_df["total_spend"].sum()
-    st.markdown(f"Total Spend: **{total_spend}**")
-
-with col2:
-    avg_spend = sum_spend_df["total_spend"].mean()
-    st.markdown("Average Spend: **{avg_spend}**")
-
-fig, ax = plt.subplots(figsize=(12, 6))
-sns.lineplot(
-    data=sum_spend_df,
-    x="order_approved_at",
-    y="total_spend",
-    marker="o",
-    linewidth=2,
-    color="#90CAF9"
-)
-
-ax.tick_params(axis="x", rotation=45)
-ax.tick_params(axis="y", labelsize=15)
-st.pyplot(fig)
-
-# Order Items
-st.subheader("Order Items")
-col1, col2 = st.columns(2)
-
-with col1:
-    total_items = sum_order_items_df["product_count"].sum()
-    st.markdown(f"Total Items: **{total_items}**")
-
-with col2:
-    avg_items = sum_order_items_df["product_count"].mean()
-    st.markdown(f"Average Items: **{avg_items}**")
-
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
-
-sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette="viridis", ax=ax[0])
-ax[0].set_ylabel(None)
-ax[0].set_xlabel("Number of Sales", fontsize=80)
-ax[0].set_title("Most sold products", loc="center", fontsize=90)
-ax[0].tick_params(axis ='y', labelsize=55)
-ax[0].tick_params(axis ='x', labelsize=50)
-
-sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette="viridis", ax=ax[1])
-ax[1].set_ylabel(None)
-ax[1].set_xlabel("Number of Sales", fontsize=80)
-ax[1].invert_xaxis()
-ax[1].yaxis.set_label_position("right")
-ax[1].yaxis.tick_right()
-ax[1].set_title("Fewest products sold", loc="center", fontsize=90)
-ax[1].tick_params(axis='y', labelsize=55)
-ax[1].tick_params(axis='x', labelsize=50)
-
-st.pyplot(fig)
-
-# Review Score
-st.subheader("Review Score")
-col1, col2 = st.columns(2)
-
-with col1:
-    avg_review_score = review_score.mean()
-    st.markdown(f"Average Review Score: **{avg_review_score:.2f}**")
-
-with col2:
-    most_common_review_score = review_score.value_counts().idxmax()
-    st.markdown(f"Most Common Review Score: **{most_common_review_score}**")
-
-fig, ax = plt.subplots(figsize=(12, 6))
-colors = sns.color_palette("viridis", len(review_score))
-
-sns.barplot(x=review_score.index,
-            y=review_score.values,
-            order=review_score.index,
-            palette=colors)
-
-plt.title("Customer Review Scores for Service", fontsize=15)
-plt.xlabel("Rating")
-plt.ylabel("Count")
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-
-# Menambahkan label di atas setiap bar
-for i, v in enumerate(review_score.values):
-    ax.text(i, v + 5, str(v), ha='center', va='bottom', fontsize=12, color='black')
-
-st.pyplot(fig)
-
-# Customer Demographic
-st.subheader("Customer Demographic")
-tab1, tab2 = st.tabs(["State", "Geolocation"])
-
-with tab1:
-    most_common_state = state.customer_state.value_counts().index[0]
-    st.markdown(f"Most Common State: **{most_common_state}**")
-
-    fig, ax = plt.subplots(figsize=(12, 6))
-    sns.barplot(x=state.customer_state.value_counts().index,
-                y=state.customer_count.values, 
-                data=state,
-                palette="viridis"
-                    )
-
-    plt.title("Number customers from State", fontsize=15)
-    plt.xlabel("State")
-    plt.ylabel("Number of Customers")
-    plt.xticks(fontsize=12)
-    st.pyplot(fig)
-
-# Fungsi untuk memformat plot (dari kode asli)
 def default_plot(ax, spines):
     ax = plt.gca()
     ax.spines["top"].set_visible(False)
@@ -283,6 +146,7 @@ plt.tight_layout()
 
 # --- Tampilkan di Streamlit ---
 st.pyplot(fig)
+
 
 
 
