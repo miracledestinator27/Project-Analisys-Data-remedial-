fdba8aeee0b73a0c1559474d1b272802005311ba
miracledestinator27 <b25b9d037@student.devacademy.id>
2025-11-11 17:50:55 +0700

    Update Dashboard2.py

--- Dashboard2.py
+++ Dashboard2.py
@@ -334,7 +334,7 @@ def plot_brazil_map(data):
 # =====================================
 # 🔹 Contoh Data Dummy
 # =====================================
-np.random.seed(50)
+np.random.seed(80)
 customers_silver = pd.DataFrame({
     'customer_unique_id': [f'U{i}' for i in range(50)],
     'geolocation_lat': np.random.uniform(-32, 38, 50),
@@ -353,6 +353,7 @@ Hal ini membantu menjaga konteks geografis dan memberikan ruang visual di sekita
 
 fig_map = plot_brazil_map(customers_silver.drop_duplicates(subset='customer_unique_id'))
 st.pyplot(fig_map)
+
 
 
 
