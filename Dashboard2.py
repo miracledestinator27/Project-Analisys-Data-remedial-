8232fafc862fe45e2ef2213e2bc1127e320a13dc
miracledestinator27 <b25b9d037@student.devacademy.id>
2025-11-11 13:59:20 +0700

    Update Dashboard2.py

--- Dashboard2.py
+++ Dashboard2.py
@@ -290,7 +290,7 @@ def plot_brazil_map(data):
     ax.scatter(
         data["geolocation_lng"],
         data["geolocation_lat"],
-        s=2,
+        s=6,
         alpha=0.8,
         color='yellow',
         edgecolor='black',
@@ -306,6 +306,7 @@ def plot_brazil_map(data):
 
 fig_map = plot_brazil_map(customers_silver.drop_duplicates(subset='customer_unique_id'))
 st.pyplot(fig_map)
+
 
 
 
