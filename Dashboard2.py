4aabb5d7ebecc0c46d6914b4cc1c626723ec0eda
miracledestinator27 <b25b9d037@student.devacademy.id>
2025-11-11 16:01:56 +0700

    Update Dashboard2.py

--- Dashboard2.py
+++ Dashboard2.py
@@ -291,8 +291,8 @@ def plot_brazil_map(data):
 
     # 🗺️ Batas peta dibuat lebih lebar dari data
     # Tambahkan margin kiri/kanan/atas/bawah agar peta tampak lebih luas
-    lon_margin = 5
-    lat_margin = 3
+    lon_margin = 7
+    lat_margin = 5
 
     map_lon_min = data_lon_min - lon_margin
     map_lon_max = data_lon_max + lon_margin
@@ -300,7 +300,7 @@ def plot_brazil_map(data):
     map_lat_max = data_lat_max + lat_margin
 
     # Buat plot
-    fig, ax = plt.subplots(figsize=(14, 7))  # lebar 2x tinggi
+    fig, ax = plt.subplots(figsize=(14, 7))  
     ax.imshow(brazil, extent=[map_lon_min, map_lon_max, map_lat_min, map_lat_max], zorder=1)
 
     # Scatter pelanggan hanya di area data aslinya
@@ -336,9 +336,9 @@ def plot_brazil_map(data):
 # =====================================
 np.random.seed(42)
 customers_silver = pd.DataFrame({
-    'customer_unique_id': [f'U{i}' for i in range(150)],
-    'geolocation_lat': np.random.uniform(-25, 2, 150),
-    'geolocation_lng': np.random.uniform(-65, -40, 150)
+    'customer_unique_id': [f'U{i}' for i in range(100)],
+    'geolocation_lat': np.random.uniform(-25, 2, 100),
+    'geolocation_lng': np.random.uniform(-65, -40, 100)
 })
 
 # =====================================
@@ -353,6 +353,7 @@ Hal ini membantu menjaga konteks geografis dan memberikan ruang visual di sekita
 
 fig_map = plot_brazil_map(customers_silver.drop_duplicates(subset='customer_unique_id'))
 st.pyplot(fig_map)
+
 
 
 
