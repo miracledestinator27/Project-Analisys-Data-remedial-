e15cb35238c28e9e74d67099ab386246d88d0118
miracledestinator27 <b25b9d037@student.devacademy.id>
2025-11-11 18:36:54 +0700

    Update Dashboard2.py

--- Dashboard2.py
+++ Dashboard2.py
@@ -291,7 +291,7 @@ def plot_brazil_map(data):
 
     # 🗺️ Batas peta dibuat lebih lebar dari data
     # Tambahkan margin kiri/kanan/atas/bawah agar peta tampak lebih luas
-    lon_margin = 16
+    lon_margin = 19
     lat_margin = 8
 
     map_lon_min = data_lon_min - lon_margin
@@ -353,6 +353,7 @@ Hal ini membantu menjaga konteks geografis dan memberikan ruang visual di sekita
 
 fig_map = plot_brazil_map(customers_silver.drop_duplicates(subset='customer_unique_id'))
 st.pyplot(fig_map)
+
 
 
 
