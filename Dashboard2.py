1f884d30d1c0971107d2c9a57192e1ccb963e86c
miracledestinator27 <b25b9d037@student.devacademy.id>
2025-11-26 01:17:45 +0700

    Update Dashboard2.py

--- Dashboard2.py
+++ Dashboard2.py
@@ -5,7 +5,7 @@ import numpy as np
 import streamlit as st
 import urllib.request
 import matplotlib.image as mpimg
-from scipy import stats as st
+from scipy import stats as stats
 
 sns.set(style='dark')
 
@@ -76,10 +76,10 @@ customer_spent = (
 
 # Mean & Std Error
 customer_mean = customer_spent['total_spent'].mean()
-customer_std = st.sem(customer_spent['total_spent'])
+customer_std = stats.sem(customer_spent['total_spent'])
 
 # Confidence interval (95%)
-ci_customer = st.t.interval(
+ci_customer = stats.t.interval(
     0.95,
     df=len(customer_spent) - 1,
     loc=customer_mean,
@@ -355,6 +355,7 @@ st.pyplot(fig)
 
 
 st.caption('Copyright (C) Mira Destiyanti 2025')
+
 
 
 
