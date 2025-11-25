f9ba6a38ec48f885927499e43a1562dc083c6e8a
miracledestinator27 <b25b9d037@student.devacademy.id>
2025-11-26 05:15:19 +0700

    Update Dashboard2.py

--- Dashboard2.py
+++ Dashboard2.py
@@ -226,8 +226,8 @@ st.markdown("## KESIMPULAN 1")
 
 st.markdown("""
 Bagaimana tren rata-rata pembelanjaan pelanggan menurut dari pesebaran kota-kota di Brazil? 
-jika dirunutkan atau dibreakdown terdapat banyak jenis-jenis produk yang telah dijual di wilayah brazil, perkembangannya bisa berbeda akan menarik lagi dari setiap kota memiliki tren produk yang berbeda-beda juga dan beragam, tidak ada hanya 1 jenis produk yang menonjol. Pada kota-kota besar cendrung memiliki lebih banyak jenis transaksi sebaliknya kebutuhan di Kota kecil tidak terlalu banyak sehingga trennya juga tidak terlihan beragam.
-analisi ini menghasilkan fakta bahwa rata-rata pengeluaran pelanggan sebesar $166.59 atau sekitar 95% dari skala Confidence Interval,
+jika dirunutkan atau dibreakdown terdapat banyak jenis-jenis produk yang telah dijual di wilayah brazil, perkembangannya bisa berbeda akan menarik lagi dari setiap kota memiliki tren produk yang berbeda-beda juga dan beragam, tidak ada hanya 1 jenis produk yang menonjol. Pada kota-kota besar cendrung memiliki lebih banyak jenis transaksi sebaliknya kebutuhan di Kota kecil tidak terlalu banyak sehingga trennya juga tidak terlihat beragam.
+analisis ini menghasilkan fakta bahwa rata-rata pengeluaran pelanggan sebesar $166.59 atau sekitar 95% dari skala Confidence Interval,
 menghasilkan total jumlah pelanggan sebanyak 96096 yang tersebar di kota-kota Brazil.
 """)
 
@@ -321,7 +321,7 @@ st.write("Berikut distribusi jumlah order kategori produk per tahun:")
 st.dataframe(category_year_counts.head(20))
 
 # --- SIDEBAR FILTER TAHUN ---
-st.sidebar.header("Filter Tahun Kategori Produk")
+st.sidebar.header("Filter Tahun Top 10 Kategori Produk")
 
 years_available = sorted(category_year_counts['year'].unique())
 
@@ -364,6 +364,7 @@ st.markdown("""
 Dinamika trasaksi E-commerce sangat beragam pada tahun 2016-2018 di Brazil, setiap tahun grafiknya pun menunjukan perbedaan tren yang berbeda berdasarkan kategori. Namun, setelah dikumpulkan informasinya dalam 3 tahun kategory produk cama_mesa_banho tetap paling diminati di beberapa kota di Brazil
 """)
 st.caption('Copyright (C) Mira Destiyanti 2025')
+
 
 
 
